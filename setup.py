e49fbdf34d391fc4e76d163c7ee255b62b03f194
Steven K <steven@pointcircle.com>
2022-10-25 20:57:59 +0000

    Version Bump [skip ci]

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-bootstrap-templatetags",
-    version="2.1.16",
+    version="2.1.17",
     description="Vanilla Bootstrap structures in simple rendering blocks",
     author="Autumn Valenta",
     author_email="steven@pivotal.energy",
