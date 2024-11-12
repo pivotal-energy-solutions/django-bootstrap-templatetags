ae7e892ab8cd7bbcd76ba95cfd80bdcd4ae34b6d
Steven K <steven@pointcircle.com>
2024-11-12 21:15:09 +0000

    Version Bump [skip ci]

--- bootstrap_templatetags/__init__.py
+++ bootstrap_templatetags/__init__.py
@@ -1,7 +1,7 @@
 __name__ = "bootstrap_templatetags"
 __author__ = "Pivotal Energy Solutions"
-__version_info__ = (2, 1, 36)
-__version__ = "2.1.36"
+__version_info__ = (2, 1, 37)
+__version__ = "2.1.37"
 __date__ = "2013/07/8 9:47:00 AM"
 
 __credits__ = ["Tim Valenta", "Steven Klass"]
