bc836ea5e4cc9dcce509bced22ce274c5893ee02
Steven K <steven@pointcircle.com>
2025-08-15 20:37:07 -0600

    Refactor workflows and coverage reporting
    Updated GitHub Actions to improve release conditions, refine coverage reporting with added precision, and streamline version bump process. Removed unused version definitions in `__init__.py`.
    
    Signed-off-by: Steven K <steven@pointcircle.com>

--- bootstrap_templatetags/__init__.py
+++ bootstrap_templatetags/__init__.py
@@ -1,7 +1,5 @@
 __name__ = "bootstrap_templatetags"
 __author__ = "Pivotal Energy Solutions"
-__version_info__ = (2, 1, 37)
-__version__ = "2.1.37"
 __date__ = "2013/07/8 9:47:00 AM"
 
 __credits__ = ["Tim Valenta", "Steven Klass"]
